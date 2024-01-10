637e70fa7ddd9c040ac4c9131d92c19750602244
Twayfrancis <tawaifrancis@gmail.com>
2024-01-10 11:05:12 +0300

    Implement a get_logger function that takes no arguments and returns a logging.Logger object.
    The logger should be named "user_data" and only log up to logging.INFO level. It should not propagate messages to other loggers. It should have a StreamHandler with RedactingFormatter as formatter.
    
    Create a tuple PII_FIELDS constant at the root of the module containing the fields from user_data.csv that are considered PII. PII_FIELDS can contain only 5 fields - choose the right list of fields that can are considered as “important” PIIs or information that you must hide in your logs. Use it to parameterize the formatter.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
-"""function called filter_datum that returns the log message obfuscated"""
-
-import re
-
+"""Module for logging data with obfuscation of sensitive fields."""
 
 import re
 import logging
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscates specified fields in a log message.
+
+    Args:
+    fields (list): A list of strings representing the field names to obfuscate.
+    redaction (str): A string representing the value to replace the field values with.
+    message (str): A string representing the log message.
+    separator (str): A string representing the character that separates fields in the log message.
+
+    Returns:
+    str: The log message with the specified fields obfuscated.
+    """
+    for field in fields:
+        message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
+    return message
 
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
@@ -24,3 +40,19 @@ class RedactingFormatter(logging.Formatter):
             message = re.sub(f"{field}=[^;]*", f"{field}={self.REDACTION}", message)
         return message
 
+def get_logger() -> logging.Logger:
+    """
+    Returns a logging.Logger object.
+
+    The logger is named "user_data" and only logs up to logging.INFO level.
+    It does not propagate messages to other loggers.
+    It has a StreamHandler with RedactingFormatter as formatter.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    sh = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    return logger
