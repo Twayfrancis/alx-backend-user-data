c317369cbbb92d2e3c0e26d334021178102e872c
Twayfrancis <tawaifrancis@gmail.com>
2024-01-17 01:56:01 +0300

    Add the method def decode_base64_authorization_header(self, base64_authorization_header: str) -> str: in the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header:
    Return None if base64_authorization_header is None
    Return None if base64_authorization_header is not a string
    Return None if base64_authorization_header is not a valid Base64 - you can use try/except
    Otherwise, return the decoded value as UTF8 string - you can use decode(utf-8)

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 ''' basic authentication file'''
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """method that decodes a base64 authrorization header"""
+        if base64_authorization_header is None or type(
+                base64_authorization_header) is not str:
+            return None
+
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            message_bytes = base64_b64decode(base64_bytes)
+            return message_bytes.decode('utf-8')
+        except Exception:
+            return None
