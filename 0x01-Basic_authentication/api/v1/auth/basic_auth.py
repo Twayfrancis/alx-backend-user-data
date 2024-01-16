6259e7ef8f3320aaf575bbe38d76772146130b57
Twayfrancis <tawaifrancis@gmail.com>
2024-01-17 01:40:10 +0300

    Add the method def extract_base64_authorization_header(self, authorization_header: str) -> str: in the class BasicAuth that returns the Base64 part of the Authorization header for a Basic Authentication:
    Return None if authorization_header is None
    Return None if authorization_header is not a string
    Return None if authorization_header doesn’t start by Basic (with a space at the end)
    Otherwise, return the value after Basic (after the space)
    You can assume authorization_header contains only one Basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ BasicAuth that inherits from Auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Method that extracts the Base64 Authorization header """
+        if authorization_header is None or type(
+                                authorization_header) is not str:
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split(' ')[1]
