6ff83f31ee841f5a5d6f8b02dc0b61ae9562c493
Twayfrancis <tawaifrancis@gmail.com>
2024-01-22 21:44:45 +0300

    SQLAlchemy model named User for a database table named users (by using the mapping declaration of SQLAlchemy).
    The model will have the following attributes:
    
    id, the integer primary key
    email, a non-nullable string
    hashed_password, a non-nullable string
    session_id, a nullable string
    reset_token, a nullable string

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Create a SQLAlchemy model named `User`
+for database table named `users`
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ SQLAlchemy model named User
+    for database table named users
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
