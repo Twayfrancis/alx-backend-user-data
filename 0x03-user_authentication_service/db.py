d602065e94a23358ba1101e816c38f1af8b41a8e
Twayfrancis <tawaifrancis@gmail.com>
2024-01-23 22:28:41 +0300

    mplement the DB.find_user_by method. This method takes in arbitrary keyword arguments and returns the first row found in the users table as filtered by the method’s input arguments. No validation of input arguments required at this point.
    Make sure that SQLAlchemy’s NoResultFound and InvalidRequestError are raised when no results are found, or when wrong query arguments are passed, respectively.
    
    Warning:
    
    NoResultFound has been moved from sqlalchemy.orm.exc to sqlalchemy.exc between the version 1.3.x and 1.4.x of SQLAchemy - please make sure you are importing it from sqlalchemy.orm.exc

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -10,8 +10,6 @@ from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
-VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
-
 
 class DB:
     """DB class
@@ -66,12 +64,14 @@ class DB:
             sqlalchemy.orm.exc.NoResultFound: if user not found
             sqlalchemy.exc.InvalidRequestError: If wrong query args passed
         """
-        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
-            raise IndentationError
-        session = self._session
-        try:
-            return self._session.query(User).filter_by(**kwargs).first()
-        except NoResultFound:
-            raise
-        except InvalidRequestError:
-            raise
+        user_keys = ['id', 'email', 'hashed_password', 'session_id',
+                     'reset_token']
+        for key in kwargs.keys():
+            if key not in user_keys:
+                raise InvalidRequestError
+        result = self._session.query(User).filter_by(**kwargs).first()
+        if result is None:
+            raise NoResultFound
+        return result
+
+        
