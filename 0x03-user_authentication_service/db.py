26c37d709cefa3cb201a89aa2a76c4a6c3ea3652
Twayfrancis <tawaifrancis@gmail.com>
2024-01-23 22:37:05 +0300

    will implement the DB.update_user method that takes as argument a required user_id integer and arbitrary keyword arguments, and returns None.
    The method will use find_user_by to locate the user to update, then will update the user’s attributes as passed in the method’s arguments then commit changes to the database.
    
    If an argument that does not correspond to a user attribute is passed, raise a ValueError.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,8 +7,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-
 from user import Base, User
+from typing import TypeVar
+
+VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
 
 
 class DB:
@@ -64,14 +66,22 @@ class DB:
             sqlalchemy.orm.exc.NoResultFound: if user not found
             sqlalchemy.exc.InvalidRequestError: If wrong query args passed
         """
-        user_keys = ['id', 'email', 'hashed_password', 'session_id',
-                     'reset_token']
-        for key in kwargs.keys():
-            if key not in user_keys:
-                raise InvalidRequestError
-        result = self._session.query(User).filter_by(**kwargs).first()
-        if result is None:
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
             raise NoResultFound
-        return result
 
-        
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        updating a user in the database
+        """
+        session = self._session
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in VALID_FIELDS:
+                raise ValueError
+            setattr(user, k, v)
+        session.commit()
